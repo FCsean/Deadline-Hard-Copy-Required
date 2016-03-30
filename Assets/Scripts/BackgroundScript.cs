12b3aa8e4ca29d78bd089fd036b0f9bcae330100
FCsean <frzsk@yahoo.com>
2016-03-30 14:44:53 +0800

    Added mountain backgound with scrolling.

---
+++ Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public float speed;
+	public GameObject otherStreet;
+
+	// Use this for initialization
+	void Start()
+	{
+		var posi = transform.position;
+		posi.x = 0;
+		transform.position = posi;
+
+		otherStreet.transform.position = new Vector2(posi.x + GetComponent<SpriteRenderer>().bounds.size.x, posi.y);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var posi = transform.position;
+		transform.position = new Vector2(posi.x - speed, posi.y);
+
+		posi = otherStreet.transform.position;
+		otherStreet.transform.position = new Vector2(posi.x - speed, posi.y);
+
+		CheckOutOfBounds(gameObject, otherStreet);
+		CheckOutOfBounds(otherStreet, gameObject);
+	}
+
+	void CheckOutOfBounds(GameObject first, GameObject second)
+	{
+		var posi = first.transform.position;
+
+		if (posi.x < 0 - GetComponent<SpriteRenderer>().bounds.size.x)
+		{
+			posi.x = second.transform.position.x + second.GetComponent<SpriteRenderer>().bounds.size.x;
+			first.transform.position = posi;
+		}
+	}
+}
